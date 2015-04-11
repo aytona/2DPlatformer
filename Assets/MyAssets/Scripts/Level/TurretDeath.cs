650d039c20224d05d9ddf2d39d5826988dd7c8c2
Christopher Aytona <christopher.aytona@hotmail.com>
2015-04-11 08:56:07 -0400

    Fixed Boss Death
    - Play Boss Death Clip Sound
    - Disable box collider

---
+++ Assets/MyAssets/Scripts/Level/TurretDeath.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretDeath : MonoBehaviour {
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.tag == "Bomb")
+		{
+			AudioManager.Instance.PlayBossDeathClip();
+			gameObject.collider2D.enabled = false;
+		}
+	}
+}
